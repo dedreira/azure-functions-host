34bf232e814075c005fb1c3ec79813d23b3e05f5
Bala <balag@outlook.com>
2020-11-20 16:24:31 -0800

    [Linux consumption] Reinitialize MSI sidecar on container restarts

--- src/WebJobs.Script.WebHost/ContainerManagement/LinuxContainerInitializationHostService.cs
+++ src/WebJobs.Script.WebHost/ContainerManagement/LinuxContainerInitializationHostService.cs
@@ -55,6 +55,15 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.ContainerManagement
 
                 bool success = _instanceManager.StartAssignment(assignmentContext);
                 _logger.LogInformation($"StartAssignment invoked (Success={success})");
+
+                if (assignmentContext.MSIContext != null)
+                {
+                    await _instanceManager.SpecializeMSISidecar(assignmentContext);
+                }
+                else
+                {
+                    _logger.LogInformation("Skipping re-specialization of MSI sidecar.");
+                }
             }
             else
             {
